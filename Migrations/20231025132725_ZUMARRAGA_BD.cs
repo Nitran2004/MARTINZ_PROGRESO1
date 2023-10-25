fba34a0fb29e708213d47103517a454b923dc579
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-10-25 08:28:01 -0500

    3 commit, migracion implementada

---
+++ Migrations/20231025132725_ZUMARRAGA_BD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MZ_PROGRESO1.Migrations
+{
+    public partial class ZUMARRAGA_BD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ZUMARRAGA",
+                columns: table => new
+                {
+                    mzCodigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    mzCalificacionFinal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    mzNombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    mzApellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    mzCorreo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    mzCodigoPostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    mzEstudianteNuevo = table.Column<bool>(type: "bit", nullable: false),
+                    mzFechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    mzPromedioConducta = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ZUMARRAGA", x => x.mzCodigo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ZUMARRAGA");
+        }
+    }
+}
